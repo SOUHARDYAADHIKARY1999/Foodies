e2146bd05863ac5c0419a6ace8d1978b01354428
SOUHARDYA <souhardyaadhikary86942@gmail.com>
2021-11-01 21:28:28 +0530

    default page added

--- Foodies_New/ui/foodies-new/src/app/app-routing.module.ts
+++ Foodies_New/ui/foodies-new/src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   
+    {path: '',loadChildren:()=>import('./home/home.module').then(m=>m.HomeModule)},
     { path: 'foods', loadChildren: () => import('./foods/foods.module').then(m => m.FoodsModule) },  
     { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
-  
     { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
       
     ];
